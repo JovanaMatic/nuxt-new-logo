3a160837b67f7a6dff05dab0a89f47d56978c202
Jovana_Matic <jovana.stankovic85@yahoo.com>
2024-05-22 22:44:15 +0200

    Create page, header and add logo

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <BaseHeader />
+  <main id="main">
+      <slot />
+    </main>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+body {
+  padding: 0;
+  margin: 0;
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div class="image-container">
+      <h3>Hello world</h3>
+      <img alt="new-nuxt-logo" src="~/assets/images/logo-uwu.png" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.container {
+  margin-top: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.image-container img {
+  margin: 0 auto;
+  display: block;
+  width: 80%;
+  height: 80%;
+}
+
+h3 {
+  font-size: 36px;
+  color: rgb(14, 168, 106);
+  text-align: center;
+  padding: 0;
+  margin: 0;
+}
+</style>
